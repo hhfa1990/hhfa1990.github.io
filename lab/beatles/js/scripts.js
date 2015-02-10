462c3328063a85f33bd013571c8d5b4e93136447
Heddie Franco <hhfa1990@gmail.com>
2015-02-10 16:47:39 -0600

    Lab
    Added the beatles CSS3 experiment

---
+++ lab/beatles/js/scripts.js
@@ -0,0 +1,22 @@
+$(document).on('ready', function () {
+    /*
+    $("#Play").click(function () {
+    $('.cabeza, .cuello, .brazo, .antebrazo, .saco, .pierna, .pantorrilla, .pie').toggleClass('paused');
+    $(this)[0].pause();
+    });*/
+
+    // $("#stop").click(function () {
+    //     $('.cabeza, .cuello, .brazo, .antebrazo, .saco, .pierna, .pantorrilla, .pie').toggleClass('paused');
+    //     $("#player")[0].pause();
+    //     $(this).css('display','none');
+    //     $('#play').css('display','initial');
+    // });
+
+    // $("#play").click(function () {
+    //     $('.cabeza, .cuello, .brazo, .antebrazo, .saco, .pierna, .pantorrilla, .pie').toggleClass('paused');
+    //     $("#player")[0].play();
+    //     $(this).css('display','none');
+    //     $('#stop').css('display','initial');
+    // });
+
+});
